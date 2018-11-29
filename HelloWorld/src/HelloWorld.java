6b19fc30e2d809e215e76e0d3745a157d0ab10be
CosminBM <cosmincoco88@gmail.com>
2018-11-29 14:07:56 +0200

    added git .idea to gitignore

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, CosminB!");
+        System.out.println("Hello, CosminB, aftyer first changing!");
 
     }
 }
