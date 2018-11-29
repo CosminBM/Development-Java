da05b844891ec76523fe8c3275fac764bd51528d
CosminBM <cosmincoco88@gmail.com>
2018-11-29 17:40:22 +0200

    Course 1

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,17 @@ public class HelloWorld {
 
         System.out.println("HelloWorld project!");
 
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int difference = 1000 - myTotal;
+
+        System.out.println(difference);
+        System.out.println(myTotal);
+
+
 
     }
 }
