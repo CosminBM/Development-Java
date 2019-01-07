18592d916f5fc9a5d851eabd1670e98dd8614ed0
CosminBM <cosmincoco88@gmail.com>
2019-01-07 15:25:55 +0200

    Methods

---
+++ Methods/src/com/cosmin/Main.java
@@ -0,0 +1,30 @@
+package com.cosmin;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final was " + finalScore);
+        }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final was " + finalScore);
+        }
+
+
+    }
+}
