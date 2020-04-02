dffb336047faaceddffc37cc7486ead93ac63f61
CosminBM <cosmincoco88@gmail.com>
2020-04-02 21:57:14 +0300

    Update3

--- DecimalComparator/src/DecimalComparator.java
+++ DecimalComparator/src/DecimalComparator.java
@@ -2,7 +2,7 @@ public class DecimalComparator {
 
     public static boolean areEqualByThreeDecimalPlaces(double one, double two) {
 
-        if((int)one * 1000 == (int)two * 1000){
+        if((int)(one * 1000) == (int)(two * 1000)){
             return true;
         } else {
             return false;
