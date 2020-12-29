6f57f1f8f682684b1c0862f0f1c587afdb561640
CosminBM <cosmincoco88@gmail.com>
2020-12-29 15:08:42 +0200

    Update 3 - Section 5

--- Section-5/src/NumberInDaysOfMonth.java
+++ Section-5/src/NumberInDaysOfMonth.java
@@ -5,42 +5,25 @@ public class NumberInDaysOfMonth {
         isLeapYear(1600);
         isLeapYear(2017);
         isLeapYear(2000);
+        getDaysInMonth(1, 2020);
+        getDaysInMonth(2, 2020);
+        getDaysInMonth(2, 2018);
+        getDaysInMonth(-1, 2020);
+        getDaysInMonth(1, -2020);
     }
 
-    public static boolean isLeapYear(int year) {
-        if (year >= 1 && year <= 9999) {
-            if (year % 4 == 0) {
-                if (year % 100 == 0) {
-                    if (year % 400 == 0) {
-                        System.out.println("true");
-                        return true;
-                    } else {
-                        System.out.println("false");
-                        return false;
-                    }
-                } else {
-                    System.out.println("false");
-                    return true;
-                }
-            } else {
-                System.out.println("false");
-                return false;
-            }
-        }
-        System.out.println("false");
-        return false;
+    public static boolean isLeapYear(int year){
+        return year >= 1 && year <= 9999 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
     }
 
-    public static int getDaysInMonth(int month, int year) {
-        if(month < 1 || month > 12){
-            return -1;
-        }
-
-        if (year < 1 || year > 9999){
-            return -1;
-        }
-     return -1;
+    public static int getDaysInMonth(int month, int year){
+        if(year < 1 || year > 9999) return -1;
+        return switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9, 11 -> 30;
+            case 2 -> isLeapYear(year) ? 29 : 28;
+            default -> -1;
+        };
     }
-
-
 }
+
