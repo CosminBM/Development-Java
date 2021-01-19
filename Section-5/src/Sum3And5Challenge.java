3f537b8ebcaeaf11979ea12c5bc6766f2134dd10
CosminBM <cosmincoco88@gmail.com>
2021-01-19 23:17:14 +0200

    Sum3And5Challenge - Section 5

---
+++ Section-5/src/Sum3And5Challenge.java
@@ -0,0 +1,18 @@
+public class Sum3And5Challenge {
+
+    public static void main(String[] args) {
+        int count = 0;
+        int sum = 0;
+        for(int i = 1; i <= 1000; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                count++;
+                System.out.println("Number that met the conditions is: " + i);
+                sum += i;
+                if(count == 5){
+                    System.out.println("Sum of the numbers that met the conditions is: " + sum);
+                    break;
+                }
+            }
+        }
+    }
+}
