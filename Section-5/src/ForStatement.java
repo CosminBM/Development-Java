ec676ccc2547f86356a9ddcd5b19fda535e5bd96
CosminBM <cosmincoco88@gmail.com>
2021-01-19 22:54:04 +0200

    ForStatement - Section 5

--- Section-5/src/ForStatement.java
+++ Section-5/src/ForStatement.java
@@ -1,22 +1,38 @@
+import java.util.Arrays;
+
 public class ForStatement {
 
     public static void main(String[] args) {
         System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
-        for (int i=2; i<=8; i++){
+        for (int i = 2; i <= 8; i++) {
             System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
         }
-        for (int i=8; i>=2; i--){
+        for (int i = 8; i >= 2; i--) {
             System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+        }
+        forMethod();
+    }
+
+    public static void forMethod() {
+        int count = 0;
+        for (int i = 10; i < 50; i++) {
+            if (isPrime(i)) {
+                System.out.println("Prime number is " + i);
+                count++;
+                if(count == 3){
+                    break;
+                }
+            }
         }
     }
 
-    public static boolean isPrime(int n){
-        if (n == 1){
+    public static boolean isPrime(int n) {
+        if (n == 1) {
             return false;
         }
 
-        for(int i=2; i<= n/2; i++){
-            if(n % i == 0){
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
@@ -24,7 +40,7 @@ public class ForStatement {
     }
 
     public static double calculateInterest(double amount, double interestRate) {
-        return(amount *(interestRate/100));
+        return (amount * (interestRate / 100));
     }
 
 }
