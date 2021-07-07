d73c901d92ae1ab67b00197721113810dde0909a
CosminBM <cosmincoco88@gmail.com>
2021-07-07 20:23:13 +0300

    Exercise: - All Factors - Perfect Number

---
+++ Section-5/src/PerfectNumber.java
@@ -0,0 +1,22 @@
+public class PerfectNumber {
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+        System.out.println(isPerfectNumber(0));
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        int sum = 0;
+        if (number < 1) {
+            return false;
+        }
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum == number;
+    }
+}
